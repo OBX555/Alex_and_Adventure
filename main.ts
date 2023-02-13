d3cfc8bc24f24a9fb19a565010ce7232865f81f9
OBX555 <82164070+OBX555@users.noreply.github.com>
2023-02-13 14:11:02 +0300

    update

--- main.ts
+++ main.ts
@@ -2,10 +2,14 @@ scene.onOverlapTile(SpriteKind.Player, sprites.dungeon.chestClosed, function (sp
     game.gameOver(true)
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
-    game.gameOver(false)
+    if (!(invulnabler)) {
+        game.gameOver(false)
+    }
 })
 let CAR: Sprite = null
 let DUCK: Sprite = null
+let invulnabler = 0
+invulnabler = 0
 tiles.setCurrentTilemap(tilemap`level 0`)
 let Alex = sprites.create(img`
     . . . . . . f f f f . . . . . . 
@@ -28,6 +32,7 @@ let Alex = sprites.create(img`
 Alex.setPosition(77, 7)
 controller.moveSprite(Alex)
 scene.cameraFollowSprite(Alex)
+music.play(music.createSong(hex`0078000408020300001c00010a006400f4016400000400000000000000000000000000050000042a0000000400012c10001400012c14001800012c28002c00012c2c003000012c30003400012c38003c00012c01001c000f05001202c102c201000405002800000064002800031400060200045f0000000400012704000800012708000c0001270c00100002272c10001400012714001800012718001c0002272a1c00200002272c2000240002272c24002800012728002c0001272c00300001273000340001273400380002272c38003c00012705001c000f0a006400f4010a00000400000000000000000000000000000000021e000000040001241000140001242000240001243000340001243c0040000124`), music.PlaybackMode.LoopingInBackground)
 game.onUpdateInterval(250, function () {
     DUCK = sprites.create(img`
         . . . . . . . . . . b 5 b . . . 
@@ -52,7 +57,7 @@ game.onUpdateInterval(250, function () {
     DUCK.x = 16
     DUCK.setFlag(SpriteFlag.DestroyOnWall, true)
 })
-game.onUpdateInterval(150, function () {
+game.onUpdateInterval(200, function () {
     CAR = sprites.create(img`
         . . . . . . . . . . . . . . . . 
         . . . . 3 3 3 3 3 3 3 3 . . . . 
